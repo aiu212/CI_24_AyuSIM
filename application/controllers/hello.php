9c079c56187f0b9ca39e45432a72946b502df8c6
septieeeee <sitiwahyunaj@gmail.com>
2026-02-24 16:08:05 +0700

    Pertemuan 2

--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -4,12 +4,12 @@ class hello extends CI_Controller {
 
     public function index()
 {
-    $data['nama'] = "Codeigneter 3 + php 7.3";
+    $data['nama'] = "Ayu dari SIM 2";
     $this->load->view('hello_view',$data);
 }
     public function namasaya()
 {
-    $data['nama'] = "Codeigneter 3 + php 7.3";
+    $data['nama'] = "Ayu dari SIM 2";
     $this->load->view('hello_view',$data);
 }
 
