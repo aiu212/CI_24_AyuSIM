567e2467e90a484da13b12d19545b5adebd173fc
ayu <sitiwahyunaj@gmail.com>
2026-03-03 15:56:59 +0700

    Pertemuan 3

--- application/models/Kategori_model.php
+++ application/models/Kategori_model.php
@@ -1,13 +1,17 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH')OR exit('No direct script access allowed');
 
-class Kategori_model extends CI_Model {
+class Kategori_model extends CI_Model{
 
     private $table ='kategori';
 
-    // Ambil semua data
+    //Ambil smeua data
     public function get_all()
     {
         return $this->db->get($this->table)->result();
+    }
+    public function insert($data)
+    {
+        return $this->db->insert($this->table,$data);
     }
 }
