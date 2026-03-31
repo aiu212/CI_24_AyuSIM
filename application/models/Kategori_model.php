a24e253f5b6485a26a7bf5b5f5315962e0c6ae27
aiu212 <sitiwahyunaj@gmail.com>
2026-03-31 15:27:51 +0700

    Pertemuan 5

--- application/models/Kategori_model.php
+++ application/models/Kategori_model.php
@@ -1,17 +1,24 @@
 <?php
-defined('BASEPATH')OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script acces allowed');
 
 class Kategori_model extends CI_Model{
 
-    private $table ='kategori';
+    private $table ='Kategori';
 
-    //Ambil smeua data
     public function get_all()
     {
         return $this->db->get($this->table)->result();
     }
     public function insert($data)
     {
-        return $this->db->insert($this->table,$data);
+        return $this->db->insert($this->table, $data);
+    }
+    public function delete($id)
+    {
+        return $this->db->delete($this->table,['id'=>$id]);
+    }
+    public function is_used($id)
+    {
+        return $this->db->where('id',$id)->count_all_results ('buku')>0;
     }
 }
