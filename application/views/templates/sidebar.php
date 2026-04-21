7df7a6b1ec6c3962a8fb5b77614c941b8e483a72
sitiwhynaj <sitiwahyunaj@gmail.com>
2026-04-21 15:37:06 +0700

    Pertemuan 8

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -10,7 +10,7 @@
 <hr class="sidebar-divider">
 
 <li class="nav-item">
-    <a class="nav-link" href="#">
+    <a class="nav-link" href="<?= site_url('dashboard'); ?>">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span>
     </a>
